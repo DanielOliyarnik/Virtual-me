78f6e460ec7c6014c5ac0814387afa3daaae690e
Callum Fletcher <Callum Fletcher>
2021-04-16 23:18:50 -0700

    basic front page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import ParticleBackground from "./ParticleBackground";
+import Matt from "./assets/Matt-removebg-preview.png";
 
 function App() {
   return (
@@ -8,16 +9,20 @@ function App() {
         <ParticleBackground />
       </div>
       <div className="content">
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            fontSize: 100,
-            fontFamily: "Continuum",
-          }}
-        >
-          <h1 className="title">VirtualMii</h1>
+        <h1 className="title">VirtualMii</h1>
+        <div className="body">
+          <div
+            className="glassmorph"
+            style={{ width: "100%", height: "fit-content" }}
+          >
+            <h6 className="body-text">
+              The innovative new solution to interactivity and engagement in
+              online classes
+            </h6>
+          </div>
+        </div>
+        <div className="mii">
+          <img src={Matt} alt="matt" />
         </div>
       </div>
     </div>
